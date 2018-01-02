fd6fd0a3c31db916fab06aab3b597a3904dd9f4e
Nick Lewis <nickeblewis@gmail.com>
2018-01-02 11:31:31 +0000

    First commit

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,26 @@
+const colors = require("../styles/colors");
+
+module.exports = {
+  pathPrefix: "/",
+  siteTitle: "Nick Lewis Photography",
+  siteLongTitle: "Nick Lewis Photoblog",
+  siteUrl: "https://nicklewis.photo",
+  siteLanguage: "en",
+  siteLogo: "/logos/logo-1024.png",
+  siteDescription: "This is the blog of photographer Nick Lewis",
+  // manifest.json
+  manifestName: "Nick Lewis Photography",
+  manifestShortName: "NickLewisPhoto",
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.first,
+  manifestThemeColor: colors.firstLight,
+  manifestDisplay: "standalone",
+  // analytics
+  analyticsTrackingId: "UA-110088221-2",
+  // author note
+  authorName: "Nick Lewis",
+  authorDescription: `Freelance photographer, web developer and writer`,
+  // texts
+  copyright:
+    "Copyright Nick Lewis 2018"
+};
